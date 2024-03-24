5768ad9f04e7668535d71b04b44301500793d257
Aster696 <aster.r@codezen.in>
2024-03-24 19:40:46 +0530

    first commit

---
+++ insertion_sort.py
@@ -0,0 +1,13 @@
+my_array = [9, 4, 2, 5, 6, 10, 3, 7, 8]
+
+def add_data():
+    print(my_array)
+    store_data=0
+    for index, array in enumerate(my_array):
+        if(index == 4):
+            store_data = array
+            my_array[index] = 1
+            my_array.append(store_data)
+    print(my_array)
+
+add_data()
---
+++ selection_sort.py
@@ -0,0 +1,7 @@
+my_array = [9, 4, 2, 5, 6, 10, 3, 7, 8]
+
+def move_lowest_to_end(): 
+    
+    print(my_array)
+
+move_lowest_to_end()
